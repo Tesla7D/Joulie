216542188409e45989b4ae7f996aa81b9e813a42
Grigory Kozyrev <grikzyrev@gmail.com>
2017-01-30 10:15:15 -0500

    Updating port for heroku

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import os
 from eve import Eve
 
 app = Eve()
+
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    port = 5000
+host = '127.0.0.1'
 
 
 @app.route("/")
@@ -19,4 +30,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(port=1337, debug=True)
+    app.run(host=host, port=port)
