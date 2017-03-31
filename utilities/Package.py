7faeb0fd512f682aed29a311ad17bf88502107b8
Grigory Kozyrev <grikzyrev@gmail.com>
2017-03-31 13:25:20 -0400

    Heroku fix

--- utilities/Package.py
+++ utilities/Package.py
@@ -3,26 +3,29 @@ import json
 
 is_local_server = False
 
-dirName = "Joulie"
+if 'IS_CLOUD' in os.environ:
+    is_local_server = False
+else:
+    dirName = "Joulie"
 
-workDir = os.path.dirname(os.path.realpath(__file__))
-index = workDir.rfind(dirName)
-if index == -1:
-    raise Exception("Unknown workng directory: " + workDir)
+    workDir = os.path.dirname(os.path.realpath(__file__))
+    index = workDir.rfind(dirName)
+    if index == -1:
+        raise Exception("Unknown workng directory: " + workDir)
 
-workDir = workDir[:index + len(dirName)]
-    
-try:
-    with open(os.path.join(workDir, 'package.json')) as data_file:
-        data = json.load(data_file)
+    workDir = workDir[:index + len(dirName)]
 
-        is_local_server = data["is_local"] in ['true', 'True', 'TRUE']
-        if is_local_server:
-            print "Running in local server mode"
-        else:
-            print "Running in remote server mode"
-except Exception:
-    print "Running in remote server mode"
+    try:
+        with open(os.path.join(workDir, 'package.json')) as data_file:
+            data = json.load(data_file)
+
+            is_local_server = data["is_local"] in ['true', 'True', 'TRUE']
+            if is_local_server:
+                print "Running in local server mode"
+            else:
+                print "Running in remote server mode"
+    except Exception:
+        print "Running in remote server mode"
 
 
 def is_local():
